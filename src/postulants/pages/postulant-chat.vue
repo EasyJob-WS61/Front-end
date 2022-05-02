1815804ec02fc155a5bd8fce1d79a31c14e43a85
ElizabethNina01 <elizabeth.nina010901@gmail.com>
2022-05-02 04:06:46 -0500

    feature: correcciones en chat

--- src/postulants/pages/postulant-chat.vue
+++ src/postulants/pages/postulant-chat.vue
@@ -2,14 +2,14 @@
 
   <v-row class="ma-0 pa-0 d-flex" >
     <v-col style="justify-content: center" class="ma-2 pa-0"><br/>
-      <v-row   style=" justify-content: center" class="ma-2 pa-0">
-      <v-card class="">
-        <v-card-title class="font-weight-bold justify-end text-h5   primary">{{ this.actual }}</v-card-title>
-      </v-card>
-      </v-row>
+      <v-app-bar> <v-row   style=" justify-content: center" class="ma-2 pa-0">
+        <v-card class="elevation-0">
+          <v-card-title class="font-weight-bold justify-end text-h5   primary">{{ this.actual }}</v-card-title>
+        </v-card>
+      </v-row></v-app-bar>
 
-      <v-row class="justify-center"   style="height: 68%;margin-top: 20px">
-        <v-col class="justify-center" cols="8" v-for="chat in chats" :key="chat">
+      <v-row class="justify-center overflow-auto"  style="height:360px">
+        <v-col class="botto" cols="8" v-for="chat in chats" :key="chat">
           <v-row class="justify-lg-start" v-if="chat.postulant_id==this.actualId && chat.applicant_id==this.applicantid && chat.fromApplicant===1">
             <v-card max-width="50%" class="black" style="background: linear-gradient(#02EDB3,#01C4FF);color: white">
              <v-card-title>{{chat.text}} </v-card-title>
@@ -27,15 +27,14 @@
         </v-col>
       </v-row>
 
-        <v-row class="justify-center" style="margin-left: 50px">
-          <v-col cols="10">
-            <v-textarea v-model="chat.text" solo name="input-7-4" label="Solo textarea"></v-textarea>
-          </v-col>
-          <v-col>
-            <v-btn @click="send()">Send</v-btn>
-          </v-col>
-        </v-row>
-
+      <v-footer app bottom align="center" class="justify-center" style="margin-left: 50px">
+        <v-col cols="10">
+          <v-textarea v-model="chat.text" solo name="input-7-4" label="Solo textarea"></v-textarea>
+        </v-col>
+        <v-col>
+          <v-btn @click="send()">Send</v-btn>
+        </v-col>
+      </v-footer>
     </v-col>
 
     <v-col cols="2" class="ma-0 pa-0 elevation-2 " style="justify-content: center;margin-top: 40px">
@@ -48,6 +47,8 @@
       </v-card>
     </v-col>
   </v-row>
+
+
 </template>
 
 <script>
@@ -57,6 +58,7 @@ import postulantsChatService from "@/postulants/services/postulants.chat.service
 
 
 import router from "@/router";
+import applicantsService from "@/applicants/services/applicants.service";
 export default {
   name: "postulant-chat",
   components: {},
@@ -66,8 +68,6 @@ export default {
     actualId:0,
     applicantid:0,
     postulants:[],
-    chatPostulants:[],
-    chatApplicants:[],
     chats:[],
     chat:{},
     items: [],
@@ -76,7 +76,9 @@ export default {
     try {
       const response1 = await ApplicantsService.getAll();
       this.applicants = response1.data;
-
+      this.actualId=this.$route.params.idUser;
+      this.applicantid=this.$route.params.idUser2;
+      await applicantsService.getById(this.applicantid).then(response=>{this.actual=response.data.name})
       const response2 = await PostulantsService.getAll();
       this.postulants = response2.data;
       const response3 = await postulantsChatService.getAll();
@@ -97,10 +99,8 @@ export default {
       this.applicantid=id;
       this.actualId=this.$route.params.idUser;
 
-      const response2 =  postulantsChatService.getByApplicantId(id);
-      this.chatApplicants = response2.data;
-      const response3 =  postulantsChatService.getByPostulantId(id);
-      this.chatPostulants = response3.data;
+      postulantsChatService.getAll().then(response=>{ this.chats= response.data});
+
     },
     send(){
       this.chat.fromApplicant=0;
@@ -108,11 +108,9 @@ export default {
       this.chat.postulant_id=this.actualId;
       this.chat.date=Date().toLocaleString();
       postulantsChatService.create(this.chat);
-      this.$router.go('/postulants/'+this.actualId+'/chat/'+this.applicantid);
-      const response2 =  postulantsChatService.getByApplicantId(this.applicantid);
-      this.chatApplicants = response2.data;
-      const response3 =  postulantsChatService.getByPostulantId(this.actualId);
-      this.chatPostulants = response3.data;
+
+      postulantsChatService.getAll().then(response=>{ this.chats= response.data});
+      this.cargarChat(this.applicantid,this.actual)
     }
   }
 }
